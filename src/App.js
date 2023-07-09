1a7f50fac22d0304c81c671645b10eecc0f6481e
Shoma-nam <132134861+Shoma-nam@users.noreply.github.com>
2023-07-09 16:22:16 +0900

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import { useContext } from "react";
+import Card from "./components/Card";
+import { AdminFlagContext } from "./components/providers/AdminFlagProvider";
+
+export const App = () => {
+  //管理者フラグ
+  const { isAdmin, setIsAdmin } = useContext(AdminFlagContext);
+
+  //「切り替え」押下時
+  const onClickSwitch = () => setIsAdmin(!isAdmin);
+
+  return (
+    <div>
+      {/*管理者フラグがtrueの時とそれ以外で文字を出しわけ */}
+      {isAdmin ? <span>管理者です</span> : <span>管理者以外です</span>}
+      <button onClick={onClickSwitch}>切り替え</button>
+      <Card isAdmin={isAdmin} />
+    </div>
+  );
+};
+export default App;
